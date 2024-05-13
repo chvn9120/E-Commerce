b90463e957754f6f50dda42c6d60024cadcedbca
Tống Đức Thành Nam <tongducthanhnam@gmail.com>
2024-05-13 17:00:21 +0700

    Ready to Deploy2

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -18,10 +18,11 @@ export interface ProvidersProps {
 
 export function Providers({children, themeProps, layout}: ProvidersProps) {
     const router = useRouter(); // This is the router
-    // @ts-ignore
     return <NextUIProvider>
         <NextThemesProvider defaultTheme="system" attribute="class" {...themeProps}>
-            {children}
+            <React.Fragment>
+                {children}
+            </React.Fragment>
         </NextThemesProvider>
     </NextUIProvider>;
 }
